b7ea1902a811cbf736c787bc80ba227702a79a56
Dennis Chmielewski <chmielewski_dennis@hotmail.com>
2021-06-04 22:00:12 +0000

    adjust initial position and zoom of map

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,9 +57,9 @@ window.onclick = function(event) {
 // GET LAT & LONG POSITION OF MAP WHEN CLICKED 
 
 function initMap() {
-    const myLatlng = { lat: -25.363, lng: 131.044 };
+    const myLatlng = { "lat": 50.04547664384781, "lng": 14.446421043851618 };
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 4.6,
       center: myLatlng,
     });
 
